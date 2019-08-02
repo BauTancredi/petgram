e39cd40ae0997165c901a3491526c54496f0e33c
Bautista Tancredi <bautistatancredi@hotmail.com>
2019-08-02 16:19:58 -0300

    Estilando el formulario

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { useInputValue } from "../../hooks/UseInputValue";
+import { Form, Input, Button, Title } from "./styles";
 
-export const UserForm = ({ onSubmit }) => {
+export const UserForm = ({ onSubmit, title }) => {
   const email = useInputValue("");
   const password = useInputValue("");
   return (
-    <form onSubmit={onSubmit}>
-      <input type="text" placeholder="Email" {...email} />
-      <input type="password" placeholder="Password" {...password} />
-      <button>Iniciar Sesión</button>
-    </form>
+    <Fragment>
+      <Title>{title}</Title>
+      <Form onSubmit={onSubmit}>
+        <Input type="text" placeholder="Email" {...email} />
+        <Input type="password" placeholder="Password" {...password} />
+        <Button>{title}</Button>
+      </Form>
+    </Fragment>
   );
 };
