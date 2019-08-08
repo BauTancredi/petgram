dee9b6aa344bca8ebdc2198ac357343dcc9116a2
Bautista Tancredi <bautistatancredi@hotmail.com>
2019-08-08 14:42:52 -0300

    PWA Offline

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifestPlugin = require("webpack-pwa-manifest");
 const path = require("path");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+
 module.exports = {
   output: {
     filename: "app.bundle.js",
@@ -20,6 +22,28 @@ module.exports = {
         {
           src: path.resolve("src/assets/icon.png"),
           sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp(
+            "https://(res.cloudinary.com|images.unsplash.com)"
+          ),
+          handler: "CacheFirst",
+          options: {
+            cacheName: "images"
+          }
+        },
+        {
+          urlPattern: new RegExp(
+            "https://petgram-sever.bautistatancredi.now.sh"
+          ),
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "api"
+          }
         }
       ]
     })
